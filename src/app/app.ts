c6741c8cf9b2f15931bf28845e2ef17b65d7b44a
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-05-15 20:22:26 +0400

    tests: Add test for App.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,6 @@ import express, { Express, Request, Response } from 'express';
 
 export const app: Express = express();
 
-app.get('/', (req: Request, res: Response): void => {
-    res.status(200).json('Hello World!');
+app.get('/', (req: Request, res: Response<{ text: string }>): void => {
+    res.status(200).json({ text: 'Hello World!' });
 });
