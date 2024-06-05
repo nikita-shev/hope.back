f9896f581f6e9ff8f9ff3d9c5a5424370b968e88
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-06-05 22:51:17 +0400

    feat: Add express.static().

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import product from '../modules/Product/Product.routes.js';
 
 export const app: Express = express();
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }));
+app.use('/public', express.static(path.join(path.parse(__dirname).dir, '..', 'public'))); // -
 
 app.use('/api/product', product);
 
