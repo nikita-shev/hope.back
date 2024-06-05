f5a5f660e00af31bb6918786534e8ea03bbbd960
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-06-05 22:35:38 +0400

    feat: Configure cors.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import product from '../modules/Product/Product.routes.js';
 
 export const app: Express = express();
 
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }));
 
 app.use('/api/product', product);
 
