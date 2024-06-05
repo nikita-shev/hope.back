f1bc7e649a98e58761ed6e73a1a6e28c44b76441
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-06-05 22:17:40 +0400

    feat: Add default image.

--- src/modules/Product/utils/methods/createImagesObj.ts
+++ src/modules/Product/utils/methods/createImagesObj.ts
@@ -1,7 +1,10 @@
 import { IImages } from '../../Product.types.js';
 import { Files } from '../../../FileStorage/FileStorage.types.js';
+import { fileStorageSettings } from '../../../FileStorage/FileStorage.settings.js';
 
-const HOST: string = process.env.HOST || `http:\\localhost:${process.env.PORT}`;
+const HOST: string = process.env.HOST || `http://localhost:${process.env.PORT}`;
+const { baseDir } = fileStorageSettings;
+const defaultImgPath: string = `${HOST}/${baseDir}/default/defaultImage.webp`;
 
 export const createImagesObj = (files: Files): IImages => {
     if (!files || !Array.isArray(files)) return { preview: '' };
@@ -9,7 +12,7 @@ export const createImagesObj = (files: Files): IImages => {
     const images: string[] = files.map((el) => `${HOST}\\${el.path}`);
 
     return {
-        preview: images[0] || 'image',
+        preview: images[0] || defaultImgPath,
         links: images.length > 1 ? images : []
     };
 };
