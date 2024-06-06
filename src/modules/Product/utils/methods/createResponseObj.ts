77d2fb9ff23ce0cb861f38de2d9ea5834959e728
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-06-07 01:05:46 +0400

    feat: Add value check to createResponseObj method.

--- src/modules/Product/utils/methods/createResponseObj.ts
+++ src/modules/Product/utils/methods/createResponseObj.ts
@@ -1,15 +1,18 @@
 import { IResponse, ResponseStatuses } from '../../../../types/Response.js';
-import { ResponseData } from '../../Product.types.js';
 
-export const createResponseObj = (
-    value: ResponseData | boolean,
+const isArray = (value: any): boolean => Array.isArray(value);
+const isNotEmptyObj = (value: any): boolean => Boolean(Object.keys(value).length);
+const isTruthy = (value: any): boolean => typeof value === 'boolean' && value;
+
+export const createResponseObj = <T>(
+    value: T,
     error: string = 'Product not found.'
-): IResponse<ResponseData> => {
+): IResponse<T | {}> => {
     const status: ResponseStatuses =
-        Object.keys(value).length || (typeof value === 'boolean' && value)
+        isNotEmptyObj(value) || isArray(value) || isTruthy(value)
             ? ResponseStatuses.OK
             : ResponseStatuses.FAILED;
-    const data: ResponseData = typeof value === 'boolean' ? {} : value;
+    const data: T | {} = typeof value === 'boolean' ? {} : value;
     const errors: string[] = status === ResponseStatuses.OK ? [] : [error];
 
     return { status, data, errors };
