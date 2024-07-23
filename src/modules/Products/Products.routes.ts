5fc6ae3c5531263acd3755f80a5f08d65d0ddd38
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-23 21:49:50 +0400

    feat[Products]: Add convertQueryParamsValues middleware.

--- src/modules/Products/Products.routes.ts
+++ src/modules/Products/Products.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { convertQueryParams } from './middleware/Products.middleware.js';
+import { convertQueryParams, convertQueryParamsValues } from './middleware/Products.middleware.js';
 import ProductsController from './Products.controller.js';
 
 const router: Router = Router();
 
-router.get('/', convertQueryParams, ProductsController.getProducts);
+router.get('/', convertQueryParams, convertQueryParamsValues, ProductsController.getProducts);
 
 export default router;
